f4462716cd17c491bcf2202cf6541b3166e4484f
David <david@DESKTOP-B6DO3DO.localdomain>
2022-04-07 00:44:49 -0500

    using local time

--- src/core/common.rs
+++ src/core/common.rs
@@ -1,4 +1,6 @@
 use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
+use chrono_tz::Tz;
+use chrono_tz::US::Central;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 pub type AppResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
@@ -11,7 +13,9 @@ pub fn get_time() -> Duration {
     return since_the_epoch;
 }
 
-pub fn parse_timestamp(timestamp: u64) -> chrono::DateTime<Utc> {
-    let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(timestamp as i64, 0), Utc);
+pub fn parse_timestamp(timestamp: u64) -> chrono::DateTime<Tz> {
+    let native_dt = NaiveDateTime::from_timestamp(timestamp as i64, 0);
+    let dt = Central.from_local_datetime(&native_dt).unwrap();
+
     return dt;
 }
